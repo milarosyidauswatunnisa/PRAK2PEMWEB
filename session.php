f483bcfcb986c6e7d22f102f1557ccf58fe066e8
milarosyidauswatunnisa <48146032+milarosyidauswatunnisa@users.noreply.github.com>
2019-06-11 21:55:06 +0700

    Add files via upload

---
+++ session.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action= "hasilform.php" methods="POST">
+Uname : <input type="text" name="uname"> <br>
+Pwd : <input type="password" name="pwd"> <br>
+<input type="submit" value="Enter">
+</form>
+
+</body>
+</html>
